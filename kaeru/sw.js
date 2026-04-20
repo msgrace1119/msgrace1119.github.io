16658b1044608a505dfb590a76e21322f01039e6
msgrace1119 <msgrace1119@users.noreply.github.com>
2026-04-20 13:14:12 +0900

    sw.js: キャッシュ無効化・常に最新版を取得

--- kaeru/sw.js
+++ kaeru/sw.js
@@ -1,20 +1,9 @@
-const CACHE = 'kaeru-v3';
-const ASSETS = ['/kaeru/', '/kaeru/index.html', '/kaeru/arrived.html', '/kaeru/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// キャッシュなし：常に最新版を取得
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
